d7c29083ee0ac28379e514af4319e804a172dd1e
valentin` <lorishuon8@gmail.com>
2024-11-09 10:01:39 +0100

    ajout code micro bit

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-
+from microbit import *
+import radio
+radio.config(group=1)
+while True :
+        message = radio.receive()
+        if message:
+            display.scroll(message)
+        if button_a.is_pressed():
+            display.scroll('A')
+        radio.send('A')
+        if button_b.is_pressed():
+            display.scroll('B')
+        radio.send('B') 
